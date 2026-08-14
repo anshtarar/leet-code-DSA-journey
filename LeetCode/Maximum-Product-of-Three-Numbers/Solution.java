33ab402b1b6c7826ed1c92ce717fa7381211b0b9
Ansh Tarar <anshtarar3@gmail.com>
2026-08-14 22:46:28 +0530

    Add solution for Maximum Product of Three Numbers

---
+++ LeetCode/Maximum-Product-of-Three-Numbers/Solution.java
@@ -0,0 +1,10 @@
+1class Solution {
+2    public int maximumProduct(int[] nums) {
+3        Arrays.sort(nums);
+4        int n = nums.length;
+5        return Math.max(
+6            nums[0] * nums[1] * nums[n - 1],
+7            nums[n - 1] * nums[n - 2] * nums[n - 3]
+8        );
+9    }
+10}
