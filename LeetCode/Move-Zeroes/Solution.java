fd2134c6c894ad1d13f335bb1522ee846367c3df
Ansh Tarar <anshtarar3@gmail.com>
2026-08-25 15:35:46 +0530

    Add solution for Move Zeroes

--- LeetCode/Move-Zeroes/Solution.java
+++ LeetCode/Move-Zeroes/Solution.java
@@ -1,14 +1,14 @@
 1class Solution {
 2    public void moveZeroes(int[] nums) {
-3        int k =0;
-4        for (int i = 0 ; i <nums.length; i++) {
-5            if (nums[i]!= 0) {
-6                nums[k] = nums[i];
-7                k++;
-8            }
-9        } while (k<nums.length){
-10            nums[k]=0;
-11            k++;
-12        }
+3        int i =0; int j =0;
+4    while (i<nums.length) {
+5        if (nums[i]!=0) {
+6            int temp = nums[i];
+7            nums[i] = nums[j];
+8            nums[j]=temp;
+9            j++;
+10        }
+11        i++;
+12    }
 13    }
 14}
