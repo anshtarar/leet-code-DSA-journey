a1c6f525e161bd94cebb2c2811087c0e29e242e7
Ansh Tarar <anshtarar3@gmail.com>
2026-09-01 09:10:07 +0530

    Add solution for Maximum Number of Vowels in a Substring of Given Length

--- LeetCode/Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length/Solution.java
+++ LeetCode/Maximum-Number-of-Vowels-in-a-Substring-of-Given-Length/Solution.java
@@ -2,12 +2,18 @@
 2    public int maxVowels(String s, int k) {
 3        int count = 0, max = 0;
 4        for (int i = 0; i < s.length(); i++) {
-5            if ("aeiou".indexOf(s.charAt(i)) != -1)
-6                count++;
-7            if (i >= k && "aeiou".indexOf(s.charAt(i - k)) != -1)
-8                count--;
-9            max = Math.max(max, count);
-10        }
-11        return max;
-12    }
-13}
+5            if (s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' ||
+6                s.charAt(i)=='o' || s.charAt(i)=='u')
+7                count++;
+8            if (i >= k) {
+9                if (s.charAt(i-k)=='a' || s.charAt(i-k)=='e' || 
+10                    s.charAt(i-k)=='i' || s.charAt(i-k)=='o' || 
+11                    s.charAt(i-k)=='u')
+12                    count--;
+13            }
+14            if (count > max)
+15                max = count;
+16        }
+17        return max;
+18    }
+19}
